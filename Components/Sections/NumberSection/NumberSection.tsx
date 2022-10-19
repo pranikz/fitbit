dabedd957338d96ba18eed6da5496a8ed89b793a
Pratyush <18btcse42@suiit.ac.in>
2022-10-20 01:23:27 +0530

    feat: added newletter commponent

--- Components/Sections/NumberSection/NumberSection.tsx
+++ Components/Sections/NumberSection/NumberSection.tsx
@@ -3,13 +3,13 @@ const NumberSection = () => {
     <div className="flex flex-col md:flex-row  gap-10 justify-around  md:h-[283px] mx-40 ">
       <div className="flex justify-center items-center p-10 hover:shadow-xl">
         <div className="flex-row justify-center">
-          <div className="text-6xl flex justify-center">50+</div>
+          <div className="text-6xl flex justify-center hover:text-buttonblue">50+</div>
           <div className="flex justify-center">outlets</div>
         </div>
       </div>
       <div className="flex justify-center items-center p-10  hover:shadow-xl">
         <div className="flex-row justify-center ">
-          <div className=" flex justify-center text-6xl text-buttonblue ">
+          <div className=" flex justify-center text-6xl hover:text-buttonblue ">
             100k+
           </div>
           <div className="flex justify-center">Students</div>
@@ -17,13 +17,13 @@ const NumberSection = () => {
       </div>
       <div className="flex justify-center items-center  p-10 hover:shadow-xl ">
         <div className="flex-row justify-center">
-          <div className="text-6xl flex justify-center">200+</div>
+          <div className="text-6xl flex justify-center hover:text-buttonblue">200+</div>
           <div className="flex justify-center">Trainers</div>
         </div>
       </div>
       <div className="flex justify-center items-center p-10 hover:shadow-xl">
         <div className="flex-row justify-center">
-          <div className="text-6xl flex justify-center">50k+</div>
+          <div className="text-6xl flex justify-center hover:text-buttonblue">50k+</div>
           <div className="flex justify-center">members</div>
         </div>
       </div>
